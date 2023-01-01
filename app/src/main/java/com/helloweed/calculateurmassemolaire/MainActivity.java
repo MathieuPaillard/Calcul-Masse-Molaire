22fc50555458c5d74f9c7701afba008dd909e537
Mathieu PAILLARD <lothard7@hotmail.fr>
2023-01-01 20:20:56 +0100

    Début ajout native ads

--- app/src/main/java/com/helloweed/calculateurmassemolaire/MainActivity.java
+++ app/src/main/java/com/helloweed/calculateurmassemolaire/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
@@ -26,6 +28,8 @@ import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.nativead.NativeAd;
+import com.google.android.gms.ads.nativead.NativeAdOptions;
 
 import java.util.Map;
 
@@ -67,6 +71,41 @@ public class MainActivity extends AppCompatActivity {
 
                 mAdView = findViewById(R.id.adView);
                 mAdView.loadAd(adRequest);
+
+
+                AdLoader adLoader = new AdLoader.Builder(MainActivity.this, "ca-app-pub-3940256099942544/2247696110")
+                        .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                            @Override
+                            public void onNativeAdLoaded(NativeAd nativeAd) {
+                                // Show the ad.
+                            }
+                        })
+                        .withAdListener(new AdListener() {
+                            @Override
+                            public void onAdFailedToLoad(LoadAdError adError) {
+                                // Handle the failure by logging, altering the UI, and so on.
+                            }
+                        })
+                        .withNativeAdOptions(new NativeAdOptions.Builder()
+                                // Methods in the NativeAdOptions.Builder class can be
+                                // used here to specify individual options settings.
+                                .build())
+                        .build();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
         });
 
